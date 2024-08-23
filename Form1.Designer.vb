b347b2b50c2077e7f1396c7709fe088a323d442e
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-08-24 00:26:18 +0800

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txt_prono = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.txt_Proname = New System.Windows.Forms.TextBox()
+        Me.txt_price = New System.Windows.Forms.TextBox()
+        Me.exp_datepicker = New System.Windows.Forms.DateTimePicker()
+        Me.status_checkbox = New System.Windows.Forms.CheckBox()
+        Me.btn_save = New System.Windows.Forms.Button()
+        Me.btn_clear = New System.Windows.Forms.Button()
+        Me.btn_delete = New System.Windows.Forms.Button()
+        Me.btn_edit = New System.Windows.Forms.Button()
+        Me.lbl_ = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.compo_proGroup = New System.Windows.Forms.ComboBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txt_search = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txt_prono
+        '
+        Me.txt_prono.Location = New System.Drawing.Point(45, 112)
+        Me.txt_prono.Name = "txt_prono"
+        Me.txt_prono.Size = New System.Drawing.Size(265, 27)
+        Me.txt_prono.TabIndex = 0
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'txt_Proname
+        '
+        Me.txt_Proname.Location = New System.Drawing.Point(45, 181)
+        Me.txt_Proname.Name = "txt_Proname"
+        Me.txt_Proname.Size = New System.Drawing.Size(265, 27)
+        Me.txt_Proname.TabIndex = 2
+        '
+        'txt_price
+        '
+        Me.txt_price.Location = New System.Drawing.Point(45, 246)
+        Me.txt_price.Name = "txt_price"
+        Me.txt_price.Size = New System.Drawing.Size(265, 27)
+        Me.txt_price.TabIndex = 3
+        '
+        'exp_datepicker
+        '
+        Me.exp_datepicker.Location = New System.Drawing.Point(45, 393)
+        Me.exp_datepicker.Name = "exp_datepicker"
+        Me.exp_datepicker.Size = New System.Drawing.Size(265, 27)
+        Me.exp_datepicker.TabIndex = 5
+        '
+        'status_checkbox
+        '
+        Me.status_checkbox.AutoSize = True
+        Me.status_checkbox.Location = New System.Drawing.Point(45, 452)
+        Me.status_checkbox.Name = "status_checkbox"
+        Me.status_checkbox.Size = New System.Drawing.Size(93, 24)
+        Me.status_checkbox.TabIndex = 6
+        Me.status_checkbox.Text = "Available"
+        Me.status_checkbox.UseVisualStyleBackColor = True
+        '
+        'btn_save
+        '
+        Me.btn_save.BackColor = System.Drawing.Color.Green
+        Me.btn_save.ForeColor = System.Drawing.Color.White
+        Me.btn_save.Location = New System.Drawing.Point(45, 506)
+        Me.btn_save.Name = "btn_save"
+        Me.btn_save.Size = New System.Drawing.Size(118, 35)
+        Me.btn_save.TabIndex = 7
+        Me.btn_save.Text = "Save"
+        Me.btn_save.UseVisualStyleBackColor = False
+        '
+        'btn_clear
+        '
+        Me.btn_clear.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.btn_clear.ForeColor = System.Drawing.Color.White
+        Me.btn_clear.Location = New System.Drawing.Point(194, 577)
+        Me.btn_clear.Name = "btn_clear"
+        Me.btn_clear.Size = New System.Drawing.Size(116, 35)
+        Me.btn_clear.TabIndex = 8
+        Me.btn_clear.Text = "Clear"
+        Me.btn_clear.UseVisualStyleBackColor = False
+        '
+        'btn_delete
+        '
+        Me.btn_delete.BackColor = System.Drawing.Color.Red
+        Me.btn_delete.ForeColor = System.Drawing.Color.White
+        Me.btn_delete.Location = New System.Drawing.Point(45, 577)
+        Me.btn_delete.Name = "btn_delete"
+        Me.btn_delete.Size = New System.Drawing.Size(118, 35)
+        Me.btn_delete.TabIndex = 9
+        Me.btn_delete.Text = "Delete"
+        Me.btn_delete.UseVisualStyleBackColor = False
+        '
+        'btn_edit
+        '
+        Me.btn_edit.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn_edit.ForeColor = System.Drawing.Color.White
+        Me.btn_edit.Location = New System.Drawing.Point(194, 506)
+        Me.btn_edit.Name = "btn_edit"
+        Me.btn_edit.Size = New System.Drawing.Size(116, 35)
+        Me.btn_edit.TabIndex = 10
+        Me.btn_edit.Text = "Update"
+        Me.btn_edit.UseVisualStyleBackColor = False
+        '
+        'lbl_
+        '
+        Me.lbl_.AutoSize = True
+        Me.lbl_.Location = New System.Drawing.Point(48, 88)
+        Me.lbl_.Name = "lbl_"
+        Me.lbl_.Size = New System.Drawing.Size(84, 20)
+        Me.lbl_.TabIndex = 11
+        Me.lbl_.Text = "Product No"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(48, 158)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 20)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Product Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(48, 223)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 20)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Price"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(48, 294)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 20)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Product Group"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(48, 370)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 20)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Expire Date"
+        '
+        'compo_proGroup
+        '
+        Me.compo_proGroup.FormattingEnabled = True
+        Me.compo_proGroup.Items.AddRange(New Object() {"SWEETS", "COOL DRINKS", "FOOD"})
+        Me.compo_proGroup.Location = New System.Drawing.Point(45, 317)
+        Me.compo_proGroup.Name = "compo_proGroup"
+        Me.compo_proGroup.Size = New System.Drawing.Size(265, 28)
+        Me.compo_proGroup.TabIndex = 16
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6})
+        Me.DataGridView1.Location = New System.Drawing.Point(366, 181)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(666, 430)
+        Me.DataGridView1.TabIndex = 17
+        '
+        'Column1
+        '
+        Me.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column1.HeaderText = "Product Number"
+        Me.Column1.MinimumWidth = 6
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        Me.Column1.Width = 147
+        '
+        'Column2
+        '
+        Me.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.Column2.HeaderText = "Product Name"
+        Me.Column2.MinimumWidth = 6
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        '
+        'Column3
+        '
+        Me.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column3.HeaderText = "Price"
+        Me.Column3.MinimumWidth = 6
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        Me.Column3.Width = 70
+        '
+        'Column4
+        '
+        Me.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column4.HeaderText = "Group"
+        Me.Column4.MinimumWidth = 6
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        Me.Column4.Width = 79
+        '
+        'Column5
+        '
+        Me.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column5.HeaderText = "Expire Date"
+        Me.Column5.MinimumWidth = 6
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        Me.Column5.Width = 115
+        '
+        'Column6
+        '
+        Me.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Column6.HeaderText = "Status"
+        Me.Column6.MinimumWidth = 6
+        Me.Column6.Name = "Column6"
+        Me.Column6.ReadOnly = True
+        Me.Column6.Width = 55
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(369, 88)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 20)
+        Me.Label5.TabIndex = 19
+        Me.Label5.Text = "Search"
+        '
+        'txt_search
+        '
+        Me.txt_search.Location = New System.Drawing.Point(366, 112)
+        Me.txt_search.Name = "txt_search"
+        Me.txt_search.Size = New System.Drawing.Size(265, 27)
+        Me.txt_search.TabIndex = 18
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1065, 777)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txt_search)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.compo_proGroup)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbl_)
+        Me.Controls.Add(Me.btn_edit)
+        Me.Controls.Add(Me.btn_delete)
+        Me.Controls.Add(Me.btn_clear)
+        Me.Controls.Add(Me.btn_save)
+        Me.Controls.Add(Me.status_checkbox)
+        Me.Controls.Add(Me.exp_datepicker)
+        Me.Controls.Add(Me.txt_price)
+        Me.Controls.Add(Me.txt_Proname)
+        Me.Controls.Add(Me.txt_prono)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CRUD MYSQL"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt_prono As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents txt_Proname As TextBox
+    Friend WithEvents txt_price As TextBox
+    Friend WithEvents exp_datepicker As DateTimePicker
+    Friend WithEvents status_checkbox As CheckBox
+    Friend WithEvents btn_save As Button
+    Friend WithEvents btn_clear As Button
+    Friend WithEvents btn_delete As Button
+    Friend WithEvents btn_edit As Button
+    Friend WithEvents lbl_ As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents compo_proGroup As ComboBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txt_search As TextBox
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewCheckBoxColumn
+End Class
---
+++ Form1.vb
@@ -0,0 +1,174 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim conn As New MySqlConnection("server=localhost; port=3306;username=root;password=;database=crud")
+    Dim i As Integer
+    Dim dr As MySqlDataReader
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DGV_load()
+    End Sub
+
+    Public Sub DGV_load()
+        DataGridView1.Rows.Clear()
+        Try
+            conn.Open()
+            Dim cmd As New MySqlCommand("SELECT * FROM tbl_crud", conn)
+            dr = cmd.ExecuteReader
+            While dr.Read
+                DataGridView1.Rows.Add(dr.Item("PRODUCTNO"), dr.Item("PRODUCTNAME"), dr.Item("PRICE"), dr.Item("GROUP"), dr.Item("EXPDATE"), Format(CBool(dr.Item("STATUS").ToString)))
+            End While
+
+
+        Catch ex As Exception
+            MsgBox("Error: " & ex.Message)
+        Finally
+            conn.Close()
+
+        End Try
+    End Sub
+
+    Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
+        save()
+        DGV_load()
+    End Sub
+
+    Public Sub save()
+        Try
+            conn.Open()
+            Dim cmd As New MySqlCommand("INSERT INTO `tbl_crud`(`PRODUCTNO`,`PRODUCTNAME`,`PRICE`,`GROUP`, `EXPDATE`, `STATUS`) VALUES (@PRODUCTNO,@PRODUCTNAME,@PRICE,@GROUP,@EXPDATE,@STATUS)", conn)
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@PRODUCTNO", txt_prono.Text)
+            cmd.Parameters.AddWithValue("@PRODUCTNAME", txt_Proname.Text)
+            cmd.Parameters.AddWithValue("@PRICE", CDec(txt_price.Text))
+            cmd.Parameters.AddWithValue("@GROUP", compo_proGroup.Text)
+            cmd.Parameters.AddWithValue("@EXPDATE", CDate(exp_datepicker.Value))
+            cmd.Parameters.AddWithValue("@STATUS", CBool(status_checkbox.Checked.ToString))
+
+            i = cmd.ExecuteNonQuery
+
+            If i > 0 Then
+                MessageBox.Show("Record Save Success!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Record Save Failed!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            End If
+        Catch ex As Exception
+            MsgBox("Error: " & ex.Message)
+        Finally
+            conn.Close()
+        End Try
+        Clear()
+    End Sub
+
+    Public Sub Clear()
+        txt_prono.Clear()
+        txt_Proname.Clear()
+        txt_price.Clear()
+        compo_proGroup.Text = ""
+        exp_datepicker.Value = Now
+        status_checkbox.CheckState = False
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        txt_prono.Text = DataGridView1.CurrentRow.Cells(0).Value
+        txt_Proname.Text = DataGridView1.CurrentRow.Cells(1).Value
+        txt_price.Text = DataGridView1.CurrentRow.Cells(2).Value
+        compo_proGroup.Text = DataGridView1.CurrentRow.Cells(3).Value
+        exp_datepicker.Text = DataGridView1.CurrentRow.Cells(4).Value
+        status_checkbox.Checked = DataGridView1.CurrentRow.Cells(5).Value
+
+        txt_prono.ReadOnly = True
+        btn_save.Enabled = False
+    End Sub
+
+    Private Sub btn_clear_Click(sender As Object, e As EventArgs) Handles btn_clear.Click
+        Clear()
+    End Sub
+
+    Public Sub Edit()
+        Try
+            conn.Open()
+            Dim cmd As New MySqlCommand("UPDATE `tbl_crud` SET `PRODUCTNAME`=@PRODUCTNAME, `PRICE`=@PRICE, `GROUP`=@GROUP, `EXPDATE`=@EXPDATE, `STATUS`=@STATUS WHERE `PRODUCTNO`=@PRODUCTNO", conn)
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@PRODUCTNO", txt_prono.Text)
+            cmd.Parameters.AddWithValue("@PRODUCTNAME", txt_Proname.Text)
+            cmd.Parameters.AddWithValue("@PRICE", CDec(txt_price.Text))
+            cmd.Parameters.AddWithValue("@GROUP", compo_proGroup.Text)
+            cmd.Parameters.AddWithValue("@EXPDATE", CDate(exp_datepicker.Value))
+            cmd.Parameters.AddWithValue("@STATUS", CBool(status_checkbox.Checked.ToString))
+
+            i = cmd.ExecuteNonQuery()
+
+            If i > 0 Then
+                MessageBox.Show("Record Update Success!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Record Update Failed!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            End If
+        Catch ex As Exception
+            MsgBox("Error: " & ex.Message)
+        Finally
+            conn.Close()
+        End Try
+        Clear()
+        DGV_load()
+    End Sub
+    Private Sub btn_edit_Click(sender As Object, e As EventArgs) Handles btn_edit.Click
+        Edit()
+        DGV_load()
+    End Sub
+
+    Public Sub delete()
+        If MsgBox("Are You sure delete this Record", MsgBoxStyle.Question + vbYesNo) = vbYes Then
+            Try
+                conn.Open()
+                Dim cmd As New MySqlCommand("DELETE FROM `tbl_crud` WHERE `PRODUCTNO`=@PRODUCTNO", conn)
+
+                cmd.Parameters.Clear()
+
+                cmd.Parameters.AddWithValue("@PRODUCTNO", txt_prono.Text)
+
+                i = cmd.ExecuteNonQuery()
+                If i > 0 Then
+                    MessageBox.Show("Record Deleted Successfully!", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    MessageBox.Show("No record found with the specified PRODUCTNO.", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            Catch ex As Exception
+
+                MsgBox("Error: " & ex.Message)
+            Finally
+                conn.Close()
+            End Try
+            Clear()
+            DGV_load()
+        Else
+            Return
+        End If
+    End Sub
+
+
+    Private Sub btn_delete_Click(sender As Object, e As EventArgs) Handles btn_delete.Click
+        delete()
+    End Sub
+
+    Private Sub txt_search_TextChanged(sender As Object, e As EventArgs) Handles txt_search.TextChanged
+
+        DataGridView1.Rows.Clear()
+        Try
+            conn.Open()
+            Dim cmd As New MySqlCommand("SELECT * FROM tbl_crud WHERE PRODUCTNO like '%" & txt_search.Text & "%' OR  PRODUCTNAME like '%" & txt_search.Text & "%' ", conn)
+            dr = cmd.ExecuteReader
+            While dr.Read
+                DataGridView1.Rows.Add(dr.Item("PRODUCTNO"), dr.Item("PRODUCTNAME"), dr.Item("PRICE"), dr.Item("GROUP"), dr.Item("EXPDATE"), Format(CBool(dr.Item("STATUS").ToString)))
+            End While
+
+
+        Catch ex As Exception
+            MsgBox("Error: " & ex.Message)
+        Finally
+            conn.Close()
+
+        End Try
+
+    End Sub
+End Class
